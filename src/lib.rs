3362ffb175703c2dc613cb18d7a919d9d27baff4
lewski <leevia00@gmail.com>
2021-11-27 03:19:59 +0200

    added basic mocking function

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,45 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+mod tests;
+
+fn start_uppercase(text: &str) -> bool {
+    let ambiguous_char: &[_] = &['i', 'I', 'l', 'L'];
+
+    match text.find(ambiguous_char) {
+        Some(index) => {
+            match text.chars().nth(index).unwrap() {
+                'i' | 'I' => index % 2 != 0,
+                _ => index % 2 == 0
+            }
+        },
+        None => false
     }
+}
+
+fn should_be_uppercase(c: char) -> Option<bool> {
+    match c {
+        'i' | 'I' => Some(false),
+        'l' | 'L' => Some(true),
+        _ => None
+    }
+}
+
+pub fn mock(text: &str) -> String {
+    
+
+    let mut uppercase = start_uppercase(text);
+
+    let mut result = String::with_capacity(text.len());
+
+    for c in text.chars() {
+        if !c.is_ascii_alphabetic() {
+            result.push(c);
+        } else if should_be_uppercase(c).unwrap_or(uppercase) {
+            result.push(c.to_ascii_uppercase());
+            uppercase = false;
+        } else {
+            result.push(c.to_ascii_lowercase());
+            uppercase = true;
+        }
+    }
+
+    result
 }
